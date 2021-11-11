9a6fce05b4b5ccfa5ac547a75b27d3259ed46d01
Padsala Tushal <57517785+padsalatushal@users.noreply.github.com>
2021-11-11 19:51:24 +0530

    add argparse for command line usages

--- WinPEsc.py
+++ WinPEsc.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+parser = ArgumentParser()
+
+parser.add_argument('-t',dest='target',required=True,help='Target to Exploit')
+parser.add_argument('-u',dest='username',required=True,help='Username to login')
+parser.add_argument('-p',dest='password',required=True,help='Password for login')
+parser.add_argument('-v','--version',help='version of this program',action='version',version='%(prog)s 1.0')
+args = parser.parse_args()
